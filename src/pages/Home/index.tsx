cd8c4e2396bdb331e50a21ae041b909bbf2c180c
Junior Alves <jrnalves@gmail.com>
2024-01-03 16:45:34 -0300

    create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from '@phosphor-icons/react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="last-task-list"
+          />
+
+          <datalist id="last-task-list">
+            <option value="job1"></option>
+            <option value="job2"></option>
+            <option value="job3"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            max={60}
+            min={5}
+            step={5}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit" disabled>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
